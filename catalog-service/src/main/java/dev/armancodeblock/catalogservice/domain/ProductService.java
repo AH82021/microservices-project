b4ef41efe0911e0a24f93a35aa62fd08628e7914
Ajmal Hakim <samimajmal@yahoo.com>
2024-04-14 15:43:49 -0400

    add catalog-service implementation

--- catalog-service/src/main/java/dev/armancodeblock/catalogservice/domain/ProductService.java
+++ catalog-service/src/main/java/dev/armancodeblock/catalogservice/domain/ProductService.java
@@ -1,6 +1,45 @@
 package dev.armancodeblock.catalogservice.domain;
 
+import dev.armancodeblock.catalogservice.ApplicationProperties;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class ProductService {}
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ApplicationProperties applicationProperties;
+
+    public ProductService(ProductRepository productRepository, ApplicationProperties applicationProperties) {
+        this.productRepository = productRepository;
+        this.applicationProperties = applicationProperties;
+    }
+
+    public PageResult<Product> getProducts(int pageNo){
+        Sort sort = Sort.by("name").ascending();
+        pageNo = pageNo <=1 ? 0 : pageNo -1;
+        Pageable pageable = PageRequest.of(pageNo, applicationProperties.pageSize(),sort);
+        Page<Product> productPage = productRepository.findAll(pageable).map(ProductMapper::toProduct);
+
+        return new PageResult<>(
+                productPage.getContent(),
+                productPage.getTotalElements(),
+                productPage.getNumber() +1,
+                productPage.getTotalPages(),
+                productPage.isFirst(),
+                productPage.isLast(),
+                productPage.hasNext(),
+                productPage.hasPrevious()
+        );
+
+    }
+    public Optional<Product> getProductByCode(String code){
+        return productRepository.findByCode(code).map(ProductMapper::toProduct);
+    }
+}
--- catalog-service/src/main/java/dev/armancodeblock/catalogservice/web/controllers/ProductController.java
+++ catalog-service/src/main/java/dev/armancodeblock/catalogservice/web/controllers/ProductController.java
@@ -1,8 +1,30 @@
 package dev.armancodeblock.catalogservice.web.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.armancodeblock.catalogservice.domain.PageResult;
+import dev.armancodeblock.catalogservice.domain.Product;
+import dev.armancodeblock.catalogservice.domain.ProductNotFoundException;
+import dev.armancodeblock.catalogservice.domain.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/products")
-public class ProductController {}
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    @GetMapping
+    PageResult<Product> getProduct(@RequestParam(name = "page",defaultValue = "1") int pageNo) {
+        return productService.getProducts(pageNo);
+    }
+@GetMapping("/{code}")
+    ResponseEntity<Product> getProductByCode(@PathVariable String code) {
+        return productService
+                .getProductByCode(code)
+                .map(ResponseEntity::ok)
+                .orElseThrow(()-> ProductNotFoundException.forCode(code));
+    }
+
+}
