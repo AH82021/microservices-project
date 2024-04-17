04bf5d48d783d91af5044e3839042e07cb7282a0
Ajmal Hakim <samimajmal@yahoo.com>
2024-04-17 09:32:18 -0400

    add test cases for ProductController

--- catalog-service/src/test/java/dev/armancodeblock/catalogservice/web/controllers/ProductControllerTest.java
+++ catalog-service/src/test/java/dev/armancodeblock/catalogservice/web/controllers/ProductControllerTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 import dev.armancodeblock.catalogservice.AbstractIT;
+import dev.armancodeblock.catalogservice.domain.Product;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
@@ -27,5 +28,31 @@ class ProductControllerTest extends AbstractIT {
                 .body("isLast", is(false))
                 .body("hasNext", is(true))
                 .body("hasPrevious", is(false));
+    }
+
+    @Test
+    void shouldGetProductByCode() {
+        Product product = given().contentType(ContentType.JSON)
+                .when()
+                .get("api/v1/products/{code}", "P100")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .extract()
+                .body()
+                .as(Product.class);
+    }
+
+    @Test
+    void shouldReturnsNotFoundWhenProductCodeNotExists() {
+        String code = "Wrong_product_code";
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("api/v1/products/{code}", code)
+                .then()
+                .statusCode(404)
+                .body("status", is(404))
+                .body("title", is("Product Not Found"))
+                .body("detail", is("Product with code " + code + " not found"));
     }
 }
