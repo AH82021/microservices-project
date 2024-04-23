7aeea989a636b483ba34a9246b3075138144868f
Ajmal Hakim <samimajmal@yahoo.com>
2024-04-23 09:47:35 -0400

    add RabbitMQ config

--- order-service/src/main/java/dev/armancodeblock/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/dev/armancodeblock/orderservice/OrderServiceApplication.java
@@ -2,8 +2,13 @@ package dev.armancodeblock.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableScheduling
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
